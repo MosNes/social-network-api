15861ec0e7ac0e6e8b43051b146ec2b1fcfae9f1
MosNes <7hekarl@gmail.com>
2022-08-27 19:44:22 -0400

    finished Thought model including Reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -3,6 +3,35 @@ const { Schema, model } = require('mongoose');
 const { DateTime } = require('luxon');
 const dateFormat = require('../utils/dateFormat');
 
+//Reaction Schema
+const ReactionSchema = new Schema({
+    //creates unique ID for each reaction using Mongoose built in ObjectId
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: DateTime.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+}
+);
+
 //Thought Schema
 const ThoughtSchema = new Schema({
     thoughtText: {
@@ -15,5 +44,28 @@ const ThoughtSchema = new Schema({
         type: Date,
         default: DateTime.now(),
         get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
     }
-})
+}
+);
+
+//counts number of reactions per thought
+ThoughtSchema.virtual('reactionCount').get(
+    function() {
+        return this.reactions.length;
+    }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
